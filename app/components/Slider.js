adba34a86006eb2f727a0208f58b57dab417ba29
MD10-25 <martindimitrov04@gmail.com>
2023-05-02 19:09:16 +0200

    slider done

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,21 +1,29 @@
 "use client"
+
 import "swiper/css";
 import "swiper/css/navigation";
+import { use } from "react"
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 
+async function getOffers(){
+    return (await fetch(`http://localhost:4000/offers`, { next: { revalidate: 30 } })).json()
+  }
+
 export default function Slider() {
+  const allOffers = use(getOffers())
+    console.log(allOffers)
     return (
-    <Swiper navigation={true} modules={[Navigation]} loop={true} className="mySwiper h-[650px] bg-slate-600" >
-        <SwiperSlide className="bg-heroBeanie bg-no-repeat bg-center bg-cover">
-            <div>
-                <h1>Orange Beanie</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda provident nulla ut, incidunt voluptas reiciendis veritatis fugiat autem cupiditate accusamus.</p>
-                <button className="uppercase">Shop now</button>
-            </div>
-        </SwiperSlide>
-        <SwiperSlide className="bg-heroBook bg-no-repeat bg-center bg-cover"></SwiperSlide>
-        <SwiperSlide className="bg-heroGirl bg-no-repeat bg-center bg-cover"></SwiperSlide>
+    <Swiper navigation={true} modules={[Navigation]} loop={true} pagination={true} className="mySwiper h-[650px] bg-slate-600" >
+            {allOffers?.map((offer, index) => (
+                <SwiperSlide className={`bg-no-repeat bg-center bg-cover ${ index === 0 ? "bg-heroBeanie" : index === allOffers.length - 1 ? "bg-heroBook" : "bg-heroGirl"}`}>
+                    <div>
+                        <h1>{offer.headline}</h1>
+                        <p>{offer.text}</p>
+                        <button className="uppercase">Shop now</button>
+                    </div>
+                </SwiperSlide>
+                ))}
     </Swiper>
     )
   }
